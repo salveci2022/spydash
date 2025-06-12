540096d212ff347e907fbb62af71aedb55a25140
Salveci dos Santos <salvecidossantos454@gmail.com>
2025-06-12 18:39:53 -0300

    Versão inicial do painel SpyDash

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="SpyDash", layout="wide")
+st.title("🕵️ SpyDash - Painel de Monitoramento")
+
+st.markdown("### Capturas em tempo real")
+st.write("Este painel exibirá capturas de teclado, áudio e prints do sistema monitorado.")
